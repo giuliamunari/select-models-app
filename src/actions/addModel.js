36ac8ac7809ee4d34e1cc7c8650bbfa35f8b9572
Astrid88 <giu.munari@gmail.com>
2019-06-22 16:48:48 +0200

    finished cleaning

--- src/actions/addModel.js
+++ src/actions/addModel.js
@@ -1,14 +1,11 @@
 export const ADD_MODEL = 'ADD_MODEL'
 
 export function addModel(model, data) {
-      
- const selected = data.find(selectedModel=> {
-    return selectedModel.name === model.name
-  })
-     return{
+    const selectedModel = data.find(selectedModel => selectedModel.name === model.name)
+    return {
         type: ADD_MODEL,
-        payload:{
-            ...selected
+        payload: {
+            ...selectedModel
         }
     }
 }
