28673ab7e0a6c06b51ef2c401ea452cad715222c
Astrid88 <giu.munari@gmail.com>
2019-06-22 15:32:23 +0200

    finished exercise 4

--- src/reducers/models.js
+++ src/reducers/models.js
@@ -1,9 +1,15 @@
 import { ADD_MODEL } from '../actions/addModel'
 
 export default (state = [], action = {}) => {
+  const condition = state.find(element=> {
+    return element.name === action.payload.name
+  })  
   switch (action.type) {
     case ADD_MODEL:
-      return action.payload
+      if (!condition){
+        return [...state, action.payload]
+      }
+      return state
     default:
       return state
   }
